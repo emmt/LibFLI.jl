e14c8b1b33c111c01595dcfd55d99a01511cef50
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2022-04-07 08:45:42 +0200

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,10 @@ using Test, LibFLI
 using LibFLI: FLIException
 
 @testset "General functions" begin
-    @test isa(LibFLI.get_lib_version(), String)
-    @test isa(LibFLI.set_debug_level("dummy", :none), Nothing)
+    @test LibFLI.get_lib_version() isa String
+    @test LibFLI.set_debug_level("dummy", :none) isa Nothing
     @test_throws FLIException LibFLI.Device("dummy")
-    @test FLI.list_devices(:serial, :camera)
+    @test FLI.list_devices(:serial, :camera) isa Nothing
 end
 
 end # module
